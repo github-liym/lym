79ab6ed9d1861bd2a7d3eadc2aa8464388a8100e
muilee-lym <lym93125>
2017-12-06 23:34:10 +0800

    搭建

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+/**
+ * Created by 94216 on 2017/6/20.
+ */
+var gulp = require('gulp'),
+	fileinclude = require('gulp-file-include'),
+	htmlBeautify = require('gulp-html-beautify'),
+	htmlmin = require('gulp-htmlmin'),
+	sass = require('gulp-Sass'),
+	browserSync = require('browser-sync').create(),
+	autoprefixer = require('gulp-autoprefixer'),
+	plumber = require('gulp-plumber'),
+	changed = require('gulp-changed'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify');
+
+
+
+gulp.task('scss',function(){
+    gulp.src(['src/**/*.scss'])
+        .pipe(plumber())
+        .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions','> 5%','Firefox < 20','> 5% in US','ie 9'],
+            cascade: false,
+            remove:false
+        }))
+        .pipe(gulp.dest('src/'))
+        .pipe(gulp.dest('dist/'));
+});
+
+
+gulp.task('watch',function () {
+    gulp.watch(['src/**/*.scss'],['scss']);
+});
+gulp.task('changed', function() {
+	return gulp.src(['src/**/assets/**/*','!src/**/assets/**/*.css','!src/**/assets/**/*.scss','!src/assets/**/*'])
+		.pipe(plumber())
+		.pipe(changed('dist'))
+		.pipe(gulp.dest('dist'));
+});
+
+// include
+gulp.task('fileinclude', function() {
+	gulp.src(['src/**/*.html','!src/**/components/*.html'])
+		.pipe(plumber())
+		.pipe(fileinclude({
+			prefix: '@@',
+			basepath: '@file'
+		}))
+		.pipe(htmlBeautify({
+			indent_size: 4,
+			indent_char: ' ',
+			unformatted: true,
+			extra_liners: []
+		}))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch',function () {
+	gulp.watch(['src/**/*.scss'],['scss']);
+	gulp.watch(['src/**/assets/**/*','!src/**/assets/**/*.css','!src/**/assets/**/*.scss'],['changed']);
+	gulp.watch(['src/**/*.html'],['fileinclude']);
+});
+
+gulp.task('browser-sync',['watch'],function(){
+	browserSync.init({
+		files: ['dist/**/*'],
+		proxy: 'localhost:80'
+	});
+});
+
+
+gulp.task('default',['browser-sync','watch',"scss"]);
