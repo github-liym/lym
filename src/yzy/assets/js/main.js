9cc1dce4accd0df5787a63c168611cb2bb9a9ba3
muilee-lym <lym93125>
2018-01-02 22:14:41 +0800

    no message

--- src/yzy/assets/js/main.js
+++ src/yzy/assets/js/main.js
@@ -1,3 +1,9 @@
 /**
  * Created by 94216 on 2017/12/6.
  */
+$(function(){
+	// active 切换
+	$(".switch-panel").on("click",".switch-item",function () {
+		$(this).addClass("active").siblings(".switch-item").removeClass("active");
+	})
+});
